7a8e99e477221e45d451c3f8c22419a0ff58e839
lkrenz <liamkrenz100@gmail.com>
2024-09-09 15:00:10 -0700

    Kept brainstorming, don't think I'm at the best solution yet.

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
 Name: YOUR NAME HERE
 
-| Date        |   Time    |                                                                                             Update |
-|:------------|:---------:|---------------------------------------------------------------------------------------------------:|
-| April 5     |  9-10pm   |                                                         I tried to write abstract, efficient code. |
+| Date        |   Time    |                                                                                                                                                                                             Update |
+|:------------|:---------:|---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| April 5     |  9-10pm   |                                                                                                                                                         I tried to write abstract, efficient code. |
 | September 9 | 2:00-2:30 | I spent this time trying to brainstorm solutions. Here's the link for the google doc I was using: https://docs.google.com/document/d/16fX3KIuMJm1qSx4kEG9SrR_zLgxet-DhEISm5DfCtiU/edit?usp=sharing |
+| September 9 | 2:30-3:00 |    Kept brainstorming, all of my brainstorming this time was in comments on the main programming file. I think I'm drawing closer to the most effective solution but I still need to do more work. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -15,6 +15,32 @@ public class HighwaysAndHospitals {
      *  hospital access for all citizens in Menlo County.
      */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
+
+
+        /**
+         * Need to find a way to separate each individual network of connectable cities.
+         * Potentially run a search algorithm on each point in the graph and see if it can reach each other point.
+         * If theres a point the graph can't reach, add every point it can to its graph.
+         * Once I have the graphs set up based off of which ones can be connected, place a hospital on every node
+         * and calculate the total price. From there, start removing hospitals starting with the node with the
+         * least number of connections and add highways to connect that node to the nearest hospital with the highest
+         * number of hospitals nearby. If that reduces the price, keep this case and move on. Once the price increases,
+         * back off of this path.
+         */
+
+        /**
+         * To separate the graph between the different colonies, pick a random point, (probably go through the array
+         * based off of how many connections each point has), and find every point it can reach through breadth first
+         * search.
+         */
+
+        /**
+         * Once you have the arrays of each colony created, you can run the cycle outlined in the section above on each
+         * individual colony to save on running efficiency. The method could be made more effecient if, instead of
+         * creating the map of individual colonies, I just keep removing hospitals until the space cannot be connected
+         * to a hospital. To do this, I could add a boolean value to to each node that says whether it is connected to a
+         * hospital. If I can't connect a space to a space that is connected to a hospital, I would backup until I could
+         */
         return 0;
     }
 }
