c418f9d0507091a168b29b9fe9a0bc4be87524f3
lkrenz <liamkrenz100@gmail.com>
2024-09-11 20:29:15 -0700

    Code works but is very inefficient.

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -22,50 +23,51 @@ public class HighwaysAndHospitals {
         if (hospitalCost < highwayCost) {
             return ((long) hospitalCost) * n;
         }
-        ArrayList<Integer>[] connections = new ArrayList[n];
+        ArrayList<Integer>[] connections = new ArrayList[n + 1];
 
-        for (int i = 0; i < cities.length; i++) {
+        for (int i=0; i< connections.length; i++) {
             connections[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < cities.length; i++) {
             connections[cities[i][0]].add(cities[i][1]);
-            connections[cities[1][1]].add(cities[i][0]);
+            connections[cities[i][1]].add(cities[i][0]);
         }
 
         int numVisited = 0;
 
-        boolean[] visited = new boolean[n];
+        boolean[] visited = new boolean[n + 1];
 
         ArrayList<Integer> clusters = new ArrayList<>();
 
         Queue<Integer> toVisit = new LinkedList<>();
 
         int numCluster = 0;
-        while (numVisited != n) {
-            int location = 0;
+        while (numVisited <= n) {
+            int location = 1;
             clusters.add(0);
-            while ((visited[location])) {
+            while ((visited[location]) && location < n) {
                 location++;
             }
             toVisit.add(location);
+            visited[location] = true;
 
             while (!toVisit.isEmpty()) {
                 location = toVisit.poll();
                 numVisited++;
                 for (int i = 0; i < connections[location].size(); i++) {
-                    if (visited[connections[location].get(i)]) {
-                        continue;
+                    if (!visited[connections[location].get(i)]) {
+                        toVisit.add(connections[location].get(i));
+                        visited[connections[location].get(i)] = true;
+                        clusters.set(numCluster, clusters.get(numCluster) + 1);
                     }
-                    toVisit.add(connections[location].get(i));
-                    visited[connections[location].get(i)] = true;
-                    clusters.set(numCluster, clusters.get(numCluster) + 1);
                 }
             }
             numCluster++;
         }
 
         long price = 0;
-
-        for (int i = 0; i < clusters.size(); i++) {
-            price += hospitalCost + (clusters.get(i) - 1) * highwayCost;
+        for (int i = 0; i < clusters.size() - 1; i++) {
+            price += hospitalCost + ((clusters.get(i)) * highwayCost);
         }
 
         return price;
